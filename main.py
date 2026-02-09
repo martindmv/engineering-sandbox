07e6bc2ede3f2e28dd6d1107db115f05aee77ed0
martindmv <martin.demerdjiev@efpedu.fr>
2026-02-09 16:25:25 +0100

    added nova groups into get method

--- main.py
+++ main.py
@@ -8,8 +8,20 @@ api = openfoodfacts.API(user_agent="MySchoolProject/1.0 (demerdjievm99@mail.com)
 
 def main():
     code = input("Enter the product code: ")
-    product = api.product.get(code, fields=["code", "product_name", "nutrition_grades"])
-    print(type(product))
+    product = api.product.get(
+        code,
+        fields=[
+            "code",
+            "product_name",
+            "nutrition_grades",
+            "nova_group",
+            "nova_groupnova-group",
+            "nova-group_100g",
+            "nove-group-serving",
+        ],
+    )
+    # type of product is dict
+    print(product)
 
 
 if __name__ == "__main__":
